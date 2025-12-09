c53d89922edd60b285b37c104a32803c354e515e
Michael Kelly <mike@membervault.co>
2025-12-09 08:04:30 -0800

    Adds initial website content and favicon
    Adds a basic website structure, including a hero section with download and Github links.
    
    Includes favicon images in various formats for cross-browser compatibility.

--- index.php
+++ index.php
@@ -5,9 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/style.css" rel="stylesheet">
+
+    <title>Nixbook OS</title>
+    <meta name="description" content="A set-and-forget modern operating system designed for simplicity, speed, and peace of mind.">
+
+    <meta property="og:title" content="Nixbook OS">
+    <meta property="og:description" content="A set-and-forget modern operating system designed for simplicity, speed, and peace of mind.">
+    <meta property="og:image" content="/images/nixbook.png">
+    <meta property="og:url" content="https://nixbookos.org">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Nixbook OS">
+    <meta name="twitter:description" content="A set-and-forget modern operating system designed for simplicity, speed, and peace of mind.">
+    <meta name="twitter:image" content="/images/nixbook.png">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/fav/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/fav/apple-touch-icon-precomposed.png">
+    <link rel="mask-icon" href="/images/fav/safari-pinned-tab.svg" color="#fff">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Nix & NixOS">
+    <link rel="icon" type="image/x-icon" href="/images/fav/favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/fav/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="/images/fav/favicon-48x48.png">
+    <link rel="icon" type="image/svg+xml" href="/images/fav/favicon.svg">
+    <meta name="msapplication-TileColor" content="#fff">
+    <meta name="msapplication-TileImage" content="/images/fav/mstile-144x144.png">
 </head>
 
-<body>
+<body class="bg-gray-900">
 
     <!---------------------------- HERO ----------------------------------------------->
     <div class="relative isolate overflow-hidden bg-gray-900">
@@ -21,19 +49,10 @@
         </svg>
         <div class="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
             <div class="mx-auto max-w-2xl lg:mx-0 lg:shrink-0 lg:pt-8">
-                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="h-11" />
-                <div class="mt-24 sm:mt-32 lg:mt-16">
-                    <a href="#" class="inline-flex space-x-6">
-                        <span class="inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300">
-                            <span>Current Version 25.11</span>
-                            <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 text-gray-500">
-                                <path d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </a>
-                </div>
+                <img src="/images/fav/safari-pinned-tab.svg" alt="Nixbook Logo" class="h-11" />
+
                 <h1 class="mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl">Nixbook OS</h1>
-                <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">A simple, modern, and self-updating Linux experience designed to just work for regular people.</p>
+                <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">A set-and-forget modern operating system designed for simplicity, speed, and peace of mind.</p>
                 <div class="mt-10 flex items-center gap-x-6">
                     <a href="https://s3.membervaultcdn.com/nixbook/nixbook-installer-25.11.iso" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Download</a>
                     <a href="https://github.com/mkellyxp/nixbook" target="_BLANK" class="text-sm/6 font-semibold text-white">View on Github <span aria-hidden="true">→</span></a>
@@ -42,66 +61,9 @@
             <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:mt-0 lg:mr-0 lg:ml-10 lg:max-w-none lg:flex-none xl:ml-32">
                 <div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                     <div class="-m-2 rounded-xl bg-white/2.5 p-2 ring-1 ring-white/10 ring-inset lg:-m-4 lg:rounded-2xl lg:p-4">
-                        <img width="2432" height="1442" src="/images/nixbook.png" alt="App screenshot" class="w-254 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
+                        <img width="2432" height="1442" src="/images/nixbook.png" alt="App screenshot" class="w-[50rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <!---------------------------- FEATURES ----------------------------------------------->
-    <div class="bg-indigo-950 py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-            <div class="mx-auto max-w-2xl lg:text-center">
-                <h2 class="text-base/7 font-semibold text-indigo-400">Deploy faster</h2>
-                <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">Everything you need to deploy your app</p>
-                <p class="mt-6 text-lg/8 text-gray-300">Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.</p>
-            </div>
-            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-                <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-                    <div class="flex flex-col">
-                        <dt class="flex items-center gap-x-3 text-base/7 font-semibold text-white">
-                            <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 flex-none text-indigo-400">
-                                <path d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765 4.5 4.5 0 0 1 8.302-3.046 3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z" clip-rule="evenodd" fill-rule="evenodd" />
-                            </svg>
-                            Push to deploy
-                        </dt>
-                        <dd class="mt-4 flex flex-auto flex-col text-base/7 text-gray-400">
-                            <p class="flex-auto">Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu.</p>
-                            <p class="mt-6">
-                                <a href="#" class="text-sm/6 font-semibold text-indigo-400 hover:text-indigo-300">Learn more <span aria-hidden="true">→</span></a>
-                            </p>
-                        </dd>
-                    </div>
-                    <div class="flex flex-col">
-                        <dt class="flex items-center gap-x-3 text-base/7 font-semibold text-white">
-                            <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 flex-none text-indigo-400">
-                                <path d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" fill-rule="evenodd" />
-                            </svg>
-                            SSL certificates
-                        </dt>
-                        <dd class="mt-4 flex flex-auto flex-col text-base/7 text-gray-400">
-                            <p class="flex-auto">Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.</p>
-                            <p class="mt-6">
-                                <a href="#" class="text-sm/6 font-semibold text-indigo-400 hover:text-indigo-300">Learn more <span aria-hidden="true">→</span></a>
-                            </p>
-                        </dd>
-                    </div>
-                    <div class="flex flex-col">
-                        <dt class="flex items-center gap-x-3 text-base/7 font-semibold text-white">
-                            <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" class="size-5 flex-none text-indigo-400">
-                                <path d="M15.312 11.424a5.5 5.5 0 0 1-9.201 2.466l-.312-.311h2.433a.75.75 0 0 0 0-1.5H3.989a.75.75 0 0 0-.75.75v4.242a.75.75 0 0 0 1.5 0v-2.43l.31.31a7 7 0 0 0 11.712-3.138.75.75 0 0 0-1.449-.39Zm1.23-3.723a.75.75 0 0 0 .219-.53V2.929a.75.75 0 0 0-1.5 0V5.36l-.31-.31A7 7 0 0 0 3.239 8.188a.75.75 0 1 0 1.448.389A5.5 5.5 0 0 1 13.89 6.11l.311.31h-2.432a.75.75 0 0 0 0 1.5h4.243a.75.75 0 0 0 .53-.219Z" clip-rule="evenodd" fill-rule="evenodd" />
-                            </svg>
-                            Simple queues
-                        </dt>
-                        <dd class="mt-4 flex flex-auto flex-col text-base/7 text-gray-400">
-                            <p class="flex-auto">Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod.</p>
-                            <p class="mt-6">
-                                <a href="#" class="text-sm/6 font-semibold text-indigo-400 hover:text-indigo-300">Learn more <span aria-hidden="true">→</span></a>
-                            </p>
-                        </dd>
-                    </div>
-                </dl>
             </div>
         </div>
     </div>
